c5dea118aae632c49d5e083cd990963156a3947d
Hoodr1ch666 <vdaniil466@gmail.com>
2024-04-10 21:54:32 +0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(".owl-carousel-full").owlCarousel({
+   margin: 20,
+   responsive: {
+      0: {
+         items: 1
+      },
+      500: {
+         items: 2
+      },
+      700: {
+         items: 3
+      },
+      1000: {
+         items: 4
+      }
+   }
+});
